42d1a37c5f7a748ee8d2255c434a675eeb6e5ecd
guilherme.pacheco <guilherfp@gmail.com>
2016-11-12 14:05:56 -0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,55 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var es = require('event-stream');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var runSequence = require('run-sequence');
+var rename = require('gulp-rename');
 
-gulp.task('default');
+gulp.task('clean', function() {
+  return gulp.src('dist/')
+    .pipe(clean());
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('uglify', function() {
+  return es.merge([ 
+      gulp.src(['bower_components/angular/angular.min.js', 'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-messages/angular-messages.min.js', 'bower_components/angular-locale-pt-br/angular-locale_pt-br.js']),
+      gulp.src(['lib/**/*.js','js/**/*.js']).pipe(concat('scripts.js')).pipe(uglify())
+    ])
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('htmlmin', function() {
+    return gulp.src('view/*.html')
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest('dist/view'));
+});
+
+gulp.task('cssmin', function() {
+    return es.merge([
+      gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css']),
+      gulp.src('css/*.css').pipe(cleanCSS())
+    ]) 
+    .pipe(concat('styles.min.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy', function() {
+    return gulp.src('index-prod.html')
+      .pipe(rename('index.html'))
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', function(cb) {
+  return runSequence('clean', ['jshint', 'uglify', 'htmlmin', 'cssmin', 'copy'], cb);
+});
